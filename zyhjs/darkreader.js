3e18ee7760647acb9a0202b71c7aed294cac0221
unknown <ddiinnggttaallkk@163.com>
2024-09-27 18:57:18 +0800

    Site updated: 2024-09-27 18:57:14

--- zyhjs/darkreader.js
+++ zyhjs/darkreader.js
@@ -4,8 +4,9 @@
 // 	else console.log(pppppp);});
 
 window.addEventListener("load", (event) => {
-  var pppppp=getComputedStyle(document.documentElement).getPropertyValue('data-theme');
-	if(pppppp=='dark')console.log("darkkkkkkkkk");
+  var p=window.getComputedStyle(document.querySelector("html"));
+  var pppppp=p.getPropertyValue("data-theme");
+	if(pppppp=="dark")console.log("darkkkkkkkkk");
 	else console.log(pppppp);
 });
 // onchange = (event) => 
