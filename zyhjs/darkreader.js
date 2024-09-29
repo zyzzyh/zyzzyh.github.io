325cd3e31e8111ba51beca54cee33554f4c880b8
unknown <ddiinnggttaallkk@163.com>
2024-09-29 21:00:40 +0800

    Site updated: 2024-09-29 21:00:36

--- zyhjs/darkreader.js
+++ zyhjs/darkreader.js
@@ -1,6 +1,13 @@
 var url = window.location.pathname;
-if(url=="/")
+if(url=="/"&&document.getElementsByTagName('html')[0].getAttribute('data-darkreader-scheme') == 'dark')
 {
-	Snackbar.show({pos: 'top-center',text: '此网站提供深色模式，可将页面向下滑动后，在右下角设置按钮处打开。', actionTextColor: '#ffff00',onActionClick: function(){	Snackbar.show({pos: 'top-center',text: '如果你在使用 darkreader 请关闭', actionTextColor: '#ffff00' });
-} });
+	// let tt=0;
+	Snackbar.show({pos: 'top-center',text: '此网站提供深色模式，可将页面向下滑动后，在右下角设置按钮处打开。\n 如果你在使用 darkreader 请关闭', actionTextColor: '#ffff00',onActionClick: function(){	
+			Snackbar.show({pos: 'top-center',text: 'hello', actionTextColor: '#ffff00' });
+			// tt=1;
+	} });
+	// if(tt==0)
+	// {
+	// 	Snackbar.show({pos: 'top-center',text: '如果你在使用 darkreader 请关闭', actionTextColor: '#ffff00' });
+	// }
 }
