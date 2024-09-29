00ca3daf1e9c72f7079a627f34c6ccc13cebbac3
unknown <ddiinnggttaallkk@163.com>
2024-09-29 21:04:28 +0800

    Site updated: 2024-09-29 21:04:23

--- zyhjs/darkreader.js
+++ zyhjs/darkreader.js
@@ -1,9 +1,10 @@
 var url = window.location.pathname;
-if(url=="/"&&document.getElementsByTagName('html')[0].getAttribute('data-darkreader-scheme') == 'dark')
+if(document.getElementsByTagName('html')[0].getAttribute('data-darkreader-scheme') == 'dark')
 {
 	// let tt=0;
 	Snackbar.show({pos: 'top-center',text: '此网站提供深色模式，可将页面向下滑动后，在右下角设置按钮处打开。\n 如果你在使用 darkreader 请关闭', actionTextColor: '#ffff00',onActionClick: function(){	
-			Snackbar.show({pos: 'top-center',text: 'hello', actionTextColor: '#ffff00' });
+			if(document.getElementsByTagName('html')[0].getAttribute('data-darkreader-scheme') != 'dark')Snackbar.show({pos: 'top-center',text: 'hello', actionTextColor: '#ffff00' });
+			else Snackbar.show({pos: 'top-center',text: '呜呜呜，快关了 darkreader 。', actionTextColor: '#ffff00' });
 			// tt=1;
 	} });
 	// if(tt==0)
